b628edb1178d70a27dd3165031ec30428646b819
Thomas Kortyka <info@toxictoast.de>
2024-05-08 12:18:23 +0200

    feat: implement health module

---
+++ apps/azkaban-gateway/src/app/health/health.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  MemoryHealthIndicator,
+  MicroserviceHealthIndicator,
+} from '@nestjs/terminus';
+import { Transport } from '@nestjs/microservices';
+
+@Controller()
+export class HealthController {
+  constructor(
+    @Inject('MEMORY_HEAP_TRESHOLD') private readonly heapTreshold: number,
+    @Inject('MEMORY_RSS_TRESHOLD') private readonly rssTreshold: number,
+    @Inject('BROKER_CONNECTION_STRING')
+    private readonly brokerConnectionString: string,
+    private readonly service: HealthCheckService,
+    private readonly memory: MemoryHealthIndicator,
+    private readonly microservices: MicroserviceHealthIndicator
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.service.check([
+      () => this.memory.checkHeap('memory_heap', this.heapTreshold),
+      () => this.memory.checkRSS('memory_rss', this.rssTreshold),
+      () =>
+        this.microservices.pingCheck('broker', {
+          transport: Transport.RMQ,
+          options: {
+            urls: [this.brokerConnectionString],
+          },
+        }),
+    ]);
+  }
+}
