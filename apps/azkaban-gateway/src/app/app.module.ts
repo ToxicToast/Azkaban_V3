1ee8c602971817deb10c2282af276f7ef13a4ca7
Thomas Kortyka <info@toxictoast.de>
2024-05-08 12:16:34 +0200

    feat: add throttler, healthmodule, metricsmodule, authmodule, usersmodule, groupsmodule, versionmodule and routermodule to app.module

--- apps/azkaban-gateway/src/app/app.module.ts
+++ apps/azkaban-gateway/src/app/app.module.ts
@@ -1,11 +1,56 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { HealthModule } from './health/health.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { UsersModule } from './users/users.module';
+import { RouterModule } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { GroupsModule } from './groups/groups.module';
+import { VersionModule } from './version/version.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        name: 'azkaban',
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    //
+    HealthModule,
+    MetricsModule,
+    AuthModule,
+    UsersModule,
+    GroupsModule,
+    VersionModule,
+    //
+    RouterModule.register([
+      {
+        path: 'health',
+        module: HealthModule,
+      },
+      {
+        path: 'metrics',
+        module: MetricsModule,
+      },
+      {
+        path: 'auth',
+        module: AuthModule,
+      },
+      {
+        path: 'users',
+        module: UsersModule,
+      },
+      {
+        path: 'groups',
+        module: GroupsModule,
+      },
+      {
+        path: 'version',
+        module: VersionModule,
+      },
+    ]),
+  ],
 })
 export class AppModule {}
