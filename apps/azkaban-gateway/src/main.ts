1f85fa6fba7e2bef0ebd09acd6e6410ed281ad1b
Thomas Kortyka <info@toxictoast.de>
2024-05-08 12:17:18 +0200

    feat: update main.ts file
    create standard way of starting nestjs based backends

--- apps/azkaban-gateway/src/main.ts
+++ apps/azkaban-gateway/src/main.ts
@@ -1,22 +1,68 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+import compression from 'compression';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function createApp(): Promise<INestApplication> {
+  return await NestFactory.create(AppModule, {
+    cors: true,
+    snapshot: true,
+    rawBody: true,
+  });
 }
 
-bootstrap();
+function configureApp(app: INestApplication): void {
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.enableShutdownHooks();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+}
+
+function addModules(app: INestApplication): void {
+  app.use(compression({}));
+  app.use(helmet());
+}
+
+function configureSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Azkaban')
+    .setDescription('Home Microservices')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .addOAuth2()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  //
+  SwaggerModule.setup('api', app, document);
+}
+
+function configureCors(app: INestApplication): void {
+  app.enableCors({
+    origin: '*',
+    maxAge: 3600,
+    optionsSuccessStatus: 200,
+  });
+}
+
+async function startApp(app: INestApplication): Promise<void> {
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(`🚀 Listening on Port: ${port}`);
+}
+
+async function bootstrap() {
+  const app = await createApp();
+  configureApp(app);
+  addModules(app);
+  configureSwagger(app);
+  configureCors(app);
+  await startApp(app);
+  Logger.log(`🚀 Azkaban-Gateway is running`);
+  Logger.log(`🚀 Version: ${process.env.APP_VERSION}`);
+}
+bootstrap().catch((err) => Logger.error(err));
