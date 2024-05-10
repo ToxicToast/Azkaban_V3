834a95954e40e7c2b5eb50d21cd84c148b541977
Thomas Kortyka <info@toxictoast.de>
2024-05-10 23:32:09 +0200

    chore: remove webhooks service

--- apps/azkaban-webhooks/src/app/webhooks/webhooks.controller.ts
+++ apps/azkaban-webhooks/src/app/webhooks/webhooks.controller.ts
@@ -1,18 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { WebhooksService } from './webhooks.service';
+import { Controller, Logger } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { NotifyTopics } from '@toxictoast/azkaban-broker-rabbitmq';
 
 @Controller()
 export class WebhooksController {
-  constructor(private readonly service: WebhooksService) {}
-
   @MessagePattern(NotifyTopics.NOTIFY)
   async notifyApiAlerts(
     @Payload('event') event: string,
     @Payload('data') data: unknown
   ): Promise<void> {
-    await this.service.sendToApiAlerts(event, data);
-    await this.service.sendToSSE(event, data);
+    Logger.debug({ event, data });
   }
 }
