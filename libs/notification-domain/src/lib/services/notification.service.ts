b333b6b749f8e672fb35119b673725b4ce59492f
Thomas Kortyka <info@toxictoast.de>
2024-05-18 21:37:34 +0200

    fix: Remove Nullable from Domain - Result fires Error Messages now

--- libs/notification-domain/src/lib/services/notification.service.ts
+++ libs/notification-domain/src/lib/services/notification.service.ts
@@ -33,12 +33,15 @@ export class NotificationService {
     }
   }
 
-  async getNotificationById(
-    id: string
-  ): Promise<Result<Nullable<NotificationAnemic>>> {
+  async getNotificationById(id: string): Promise<Result<NotificationAnemic>> {
     try {
       const result = await this.repository.findById(id);
-      return Result.ok<Nullable<NotificationAnemic>>(result);
+      if (result !== null) {
+        return Result.ok<Nullable<NotificationAnemic>>(result);
+      }
+      return Result.fail<Nullable<NotificationAnemic>>(
+        'Notification not found'
+      );
     } catch (error) {
       return Result.fail<Nullable<NotificationAnemic>>(error);
     }
@@ -49,7 +52,10 @@ export class NotificationService {
   ): Promise<Result<Array<NotificationAnemic>>> {
     try {
       const result = await this.repository.findByService(service);
-      return Result.ok<Array<NotificationAnemic>>(result);
+      if (result.length > 0) {
+        return Result.ok<Array<NotificationAnemic>>(result);
+      }
+      return Result.fail<Array<NotificationAnemic>>('Notification not found');
     } catch (error) {
       return Result.fail<Array<NotificationAnemic>>(error);
     }
@@ -60,7 +66,10 @@ export class NotificationService {
   ): Promise<Result<Array<NotificationAnemic>>> {
     try {
       const result = await this.repository.findByEvent(event);
-      return Result.ok<Array<NotificationAnemic>>(result);
+      if (result.length > 0) {
+        return Result.ok<Array<NotificationAnemic>>(result);
+      }
+      return Result.fail<Array<NotificationAnemic>>('Notification not found');
     } catch (error) {
       return Result.fail<Array<NotificationAnemic>>(error);
     }
@@ -77,39 +86,33 @@ export class NotificationService {
     }
   }
 
-  async deleteNotification(
-    id: string
-  ): Promise<Result<Nullable<NotificationAnemic>>> {
+  async deleteNotification(id: string): Promise<Result<NotificationAnemic>> {
     try {
       const notification = await this.getNotificationById(id);
-      if (notification !== null) {
+      if (notification.isSuccess) {
         const notificationValue = notification.value;
         const aggregate = this.factory.reconstitute(notificationValue);
         aggregate.delete();
         return await this.save(aggregate.toAnemic());
       }
-      return Result.fail<Nullable<NotificationAnemic>>(
-        'Notification not found'
-      );
+      return Result.fail<NotificationAnemic>('Notification not found');
     } catch (error) {
-      return Result.fail<Nullable<NotificationAnemic>>(error);
+      return Result.fail<NotificationAnemic>(error);
     }
   }
 
   async restoreNotification(id: string): Promise<Result<NotificationAnemic>> {
     try {
       const notification = await this.getNotificationById(id);
-      if (notification !== null) {
+      if (notification.isSuccess) {
         const notificationValue = notification.value;
         const aggregate = this.factory.reconstitute(notificationValue);
         aggregate.restore();
         return await this.save(aggregate.toAnemic());
       }
-      return Result.fail<Nullable<NotificationAnemic>>(
-        'Notification not found'
-      );
+      return Result.fail<NotificationAnemic>('Notification not found');
     } catch (error) {
-      return Result.fail<Nullable<NotificationAnemic>>(error);
+      return Result.fail<NotificationAnemic>(error);
     }
   }
 }
