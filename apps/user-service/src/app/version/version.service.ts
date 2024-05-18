effab1f30be4b161e9e947a8f110b4c743a3eb3a
Thomas Kortyka <info@toxictoast.de>
2024-05-19 00:45:43 +0200

    chore: add metrics, health and version module to users

---
+++ apps/user-service/src/app/version/version.service.ts
@@ -0,0 +1,10 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class VersionService {
+  constructor(@Inject('APP_VERSION') private readonly appVersion: string) {}
+
+  getVersion(): string {
+    return this.appVersion;
+  }
+}
