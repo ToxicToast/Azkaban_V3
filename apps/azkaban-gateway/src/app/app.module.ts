67a614bfc7c35ea26dda60cc6568a7975cf6bd22
Thomas Kortyka <info@toxictoast.de>
2024-05-18 23:21:45 +0200

    chore: remove unused modules

--- apps/azkaban-gateway/src/app/app.module.ts
+++ apps/azkaban-gateway/src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
 import { MetricsModule } from './metrics/metrics.module';
-import { UsersModule } from './users/users.module';
 import { RouterModule } from '@nestjs/core';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { GroupsModule } from './groups/groups.module';
 import { VersionModule } from './version/version.module';
 
 @Module({
@@ -20,9 +17,6 @@ import { VersionModule } from './version/version.module';
     //
     HealthModule,
     MetricsModule,
-    AuthModule,
-    UsersModule,
-    GroupsModule,
     VersionModule,
     //
     RouterModule.register([
@@ -33,18 +27,6 @@ import { VersionModule } from './version/version.module';
       {
         path: 'metrics',
         module: MetricsModule,
-      },
-      {
-        path: 'auth',
-        module: AuthModule,
-      },
-      {
-        path: 'users',
-        module: UsersModule,
-      },
-      {
-        path: 'groups',
-        module: GroupsModule,
       },
       {
         path: 'version',
