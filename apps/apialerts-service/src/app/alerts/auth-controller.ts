8836e81b6c4d6d9806991e595bff9c011144ec2d
Thomas Kortyka <info@toxictoast.de>
2024-05-22 01:52:10 +0200

    chore: remove on use deactivated, deleted and restored events

--- apps/apialerts-service/src/app/alerts/auth-controller.ts
+++ apps/apialerts-service/src/app/alerts/auth-controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Logger } from '@nestjs/common';
 import { AuthAlertsService } from './auth-alerts.service';
 import { EventPattern } from '@nestjs/microservices';
-import { NotifyTopics } from '@toxictoast/azkaban-broker-rabbitmq';
+import { AuthTopics, NotifyTopics } from '@toxictoast/azkaban-broker-rabbitmq';
 
 @Controller('alerts/auth')
 export class AuthController {
@@ -10,7 +10,7 @@ export class AuthController {
   @EventPattern(NotifyTopics.APIALERTS)
   onUserCreated(alert: { event: string; data: unknown }): void {
     const { event, data } = alert;
-    if (event === 'user-created') {
+    if (event === AuthTopics.REGISTER) {
       Logger.debug({ event, data }, AuthController.name);
       this.authAlerts.onUserCreated(data);
     }
@@ -18,39 +18,9 @@ export class AuthController {
   }
 
   @EventPattern(NotifyTopics.APIALERTS)
-  onUserDeactivated(alert: { event: string; data: unknown }): void {
-    const { event, data } = alert;
-    if (event === 'user-deactivated') {
-      Logger.debug({ event, data }, AuthController.name);
-      this.authAlerts.onUserDeactivated(data);
-    }
-    return null;
-  }
-
-  @EventPattern(NotifyTopics.APIALERTS)
-  onUserDeleted(alert: { event: string; data: unknown }): void {
-    const { event, data } = alert;
-    if (event === 'user-deleted') {
-      Logger.debug({ event, data }, AuthController.name);
-      this.authAlerts.onUserDeleted(data);
-    }
-    return null;
-  }
-
-  @EventPattern(NotifyTopics.APIALERTS)
-  onUserRestored(alert: { event: string; data: unknown }): void {
-    const { event, data } = alert;
-    if (event === 'user-restored') {
-      Logger.debug({ event, data }, AuthController.name);
-      this.authAlerts.onUserRestored(data);
-    }
-    return null;
-  }
-
-  @EventPattern(NotifyTopics.APIALERTS)
   onUserLogin(alert: { event: string; data: unknown }): void {
     const { event, data } = alert;
-    if (event === 'user-login') {
+    if (event === AuthTopics.LOGIN) {
       Logger.debug({ event, data }, AuthController.name);
       this.authAlerts.onUserLogin(data);
     }
