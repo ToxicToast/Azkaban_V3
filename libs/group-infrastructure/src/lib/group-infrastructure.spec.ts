738af5f9bd571e54b549bb2b687d567d153c486f
Thomas Kortyka <info@toxictoast.de>
2024-06-02 21:41:57 +0200

    feat: init group infra

---
+++ libs/group-infrastructure/src/lib/group-infrastructure.spec.ts
@@ -0,0 +1,7 @@
+import { groupInfrastructure } from './group-infrastructure';
+
+describe('groupInfrastructure', () => {
+  it('should work', () => {
+    expect(groupInfrastructure()).toEqual('group-infrastructure');
+  });
+});
