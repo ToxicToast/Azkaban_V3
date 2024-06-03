fab0383bd29996036059ef2bac8f329ef15e47c1
Thomas Kortyka <info@toxictoast.de>
2024-06-03 15:31:39 +0200

    feat: onGroupCreated Alarm

---
+++ apps/apialerts-service/src/app/alerts/group-controller.ts
@@ -0,0 +1,22 @@
+import { Controller } from '@nestjs/common';
+import { AuthAlertsService } from './auth-alerts.service';
+import { EventPattern } from '@nestjs/microservices';
+import {
+  GroupsTopics,
+  NotifyTopics,
+} from '@toxictoast/azkaban-broker-rabbitmq';
+import { GroupAlertsService } from './group-alerts.service';
+
+@Controller('alerts/group')
+export class GroupController {
+  constructor(private readonly groupAlerts: GroupAlertsService) {}
+
+  @EventPattern(NotifyTopics.APIALERTS)
+  onGroupCreated(alert: { event: string; data: unknown }): void {
+    const { event, data } = alert;
+    if (event === GroupsTopics.CREATE) {
+      this.groupAlerts.onGroupCreated(data);
+    }
+    return null;
+  }
+}
