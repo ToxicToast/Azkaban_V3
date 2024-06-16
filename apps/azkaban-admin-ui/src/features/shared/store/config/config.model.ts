3882788055885709b8567823f88f2493df73d05e
Thomas Kortyka <info@toxictoast.de>
2024-06-16 04:53:58 +0200

    feat: add version to config

--- apps/azkaban-admin-ui/src/features/shared/store/config/config.model.ts
+++ apps/azkaban-admin-ui/src/features/shared/store/config/config.model.ts
@@ -1,3 +1,4 @@
 export interface ConfigModel {
     baseUrl: string;
+    version: string;
 }
--- apps/azkaban-admin-ui/src/features/shared/store/config/config.state.ts
+++ apps/azkaban-admin-ui/src/features/shared/store/config/config.state.ts
@@ -1,5 +1,6 @@
 import { ConfigModel } from './config.model';
 
 export const configState: ConfigModel = {
-    baseUrl: import.meta.env['VITE_AZKABAN_BASE_URL'],
+    baseUrl: import.meta.env['VITE_AZKABAN_BASE_URL'] ?? '',
+    version: import.meta.env['VITE_AZKABAN_VERSION'] ?? 'v0.0.0',
 };
