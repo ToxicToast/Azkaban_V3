88afb975ea8aad662791f1b79ecfe08591e4b487
Thomas Kortyka <info@toxictoast.de>
2024-06-16 04:53:45 +0200

    chore: remove config reducer

--- apps/azkaban-admin-ui/src/features/shared/store/config/config.reducer.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigModel } from './config.model';
-import { PayloadAction } from '@reduxjs/toolkit';
-
-export function setBaseUrlAction(
-    state: ConfigModel,
-    action: PayloadAction<string>,
-) {
-    state.baseUrl = action.payload;
-}
