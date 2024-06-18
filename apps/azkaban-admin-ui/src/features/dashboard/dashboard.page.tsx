b86eefb51b736d5f24914a9cbabb6c04c11cc72c
Thomas Kortyka <info@toxictoast.de>
2024-06-18 12:52:23 +0200

    feat: remove cards with admin statistics

--- apps/azkaban-admin-ui/src/features/dashboard/dashboard.page.tsx
+++ apps/azkaban-admin-ui/src/features/dashboard/dashboard.page.tsx
@@ -1,32 +1,30 @@
 import { Stats } from './components';
-import { DollarSign, Users, CreditCard, Activity } from 'lucide-react';
+import {
+    DollarSign,
+    Users,
+    CreditCard,
+    Activity,
+    Group,
+    Notebook,
+} from 'lucide-react';
 
 function DashboardPage() {
     return (
         <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
             <Stats
-                title="Total Revenue"
-                icon={<DollarSign className="h-4 w-4 text-muted-foreground" />}
-                statistic="$45,231.89"
-                fromLastMonth="+20.1%"
+                title="Total Notifications"
+                icon={<Notebook className="h-4 w-4 text-muted-foreground" />}
+                statistic="0"
             />
             <Stats
-                title="Subscriptions"
+                title="Total Users"
                 icon={<Users className="h-4 w-4 text-muted-foreground" />}
-                statistic="+2350"
-                fromLastMonth="+180.1%"
+                statistic="0"
             />
             <Stats
-                title="Sales"
-                icon={<CreditCard className="h-4 w-4 text-muted-foreground" />}
-                statistic="+12,234"
-                fromLastMonth="+19%"
-            />
-            <Stats
-                title="Active Now"
-                icon={<Activity className="h-4 w-4 text-muted-foreground" />}
-                statistic="+573"
-                fromLastMonth="+201"
+                title="Total Groups"
+                icon={<Group className="h-4 w-4 text-muted-foreground" />}
+                statistic="0"
             />
         </div>
     );
