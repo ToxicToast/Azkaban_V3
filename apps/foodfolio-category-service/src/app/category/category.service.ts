4e06681bfb16ea348f377355524a9fc87d01e8cb
Thomas Kortyka <info@toxictoast.de>
2024-06-19 14:24:26 +0200

    feat: add category service and call service in controller

---
+++ apps/foodfolio-category-service/src/app/category/category.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { Optional } from '@toxictoast/azkaban-base-types';
+
+@Injectable()
+export class CategoryService {
+    constructor() {}
+
+    async getList(limit: number, offset: number): Promise<Array<any>> {
+        return [];
+    }
+
+    async getById(id: string): Promise<any> {
+        return {};
+    }
+
+    async getByParentId(parent_id: string): Promise<Array<any>> {
+        return [];
+    }
+
+    async createCategory(
+        title: string,
+        parent_id?: Optional<string>,
+    ): Promise<any> {
+        return {};
+    }
+
+    async updateCategory(
+        id: string,
+        title?: Optional<string>,
+        parent_id?: Optional<string>,
+        activated_at?: Optional<Date>,
+    ): Promise<any> {
+        if (title !== undefined) {
+            //
+        }
+        if (parent_id !== undefined) {
+            //
+        }
+        if (activated_at !== undefined) {
+            //
+        }
+    }
+
+    async deleteCategory(id: string): Promise<any> {
+        return {};
+    }
+
+    async restoreCategory(id: string): Promise<any> {
+        return {};
+    }
+}
