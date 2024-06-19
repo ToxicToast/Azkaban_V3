d2bdd369dd5ff7f82ce248731a548f3c5de2e5e6
Thomas Kortyka <info@toxictoast.de>
2024-06-19 14:25:05 +0200

    fix: version controller in category had the wrong topic

--- apps/foodfolio-category-service/src/app/version/version.controller.ts
+++ apps/foodfolio-category-service/src/app/version/version.controller.ts
@@ -1,13 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { VersionService } from './version.service';
 import { MessagePattern } from '@nestjs/microservices';
-import { AuthTopics } from '@toxictoast/azkaban-broker-rabbitmq';
+import { FoodfolioCategoryTopics } from '@toxictoast/azkaban-broker-rabbitmq';
 
 @Controller('version')
 export class VersionController {
     constructor(private readonly service: VersionService) {}
 
-    @MessagePattern(AuthTopics.VERSION)
+    @MessagePattern(FoodfolioCategoryTopics.VERSION)
     async getVersion() {
         return this.service.getVersion();
     }
