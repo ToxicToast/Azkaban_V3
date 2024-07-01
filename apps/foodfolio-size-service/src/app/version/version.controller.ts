7fdf08183e33ab728f60007658aff3acff5cd33d
Thomas Kortyka <info@toxictoast.de>
2024-07-01 14:35:59 +0200

    fix: version

--- apps/foodfolio-size-service/src/app/version/version.controller.ts
+++ apps/foodfolio-size-service/src/app/version/version.controller.ts
@@ -1,13 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { VersionService } from './version.service';
 import { MessagePattern } from '@nestjs/microservices';
-import { FoodfolioCategoryTopics } from '@toxictoast/azkaban-broker-rabbitmq';
+import { FoodfolioSizeTopics } from '@toxictoast/azkaban-broker-rabbitmq';
 
 @Controller('version')
 export class VersionController {
     constructor(private readonly service: VersionService) {}
 
-    @MessagePattern(FoodfolioCategoryTopics.VERSION)
+    @MessagePattern(FoodfolioSizeTopics.VERSION)
     async getVersion() {
         return this.service.getVersion();
     }
--- apps/foodfolio-type-service/src/app/version/version.controller.ts
+++ apps/foodfolio-type-service/src/app/version/version.controller.ts
@@ -1,13 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { VersionService } from './version.service';
 import { MessagePattern } from '@nestjs/microservices';
-import { FoodfolioCategoryTopics } from '@toxictoast/azkaban-broker-rabbitmq';
+import { FoodfolioTypeTopics } from '@toxictoast/azkaban-broker-rabbitmq';
 
 @Controller('version')
 export class VersionController {
     constructor(private readonly service: VersionService) {}
 
-    @MessagePattern(FoodfolioCategoryTopics.VERSION)
+    @MessagePattern(FoodfolioTypeTopics.VERSION)
     async getVersion() {
         return this.service.getVersion();
     }
