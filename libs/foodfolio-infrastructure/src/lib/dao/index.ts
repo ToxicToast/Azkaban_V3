7b2a7a1d015f330259fa5fb79e677c586061ca25
Thomas Kortyka <info@toxictoast.de>
2024-07-01 09:16:39 +0200

    feat: add DAO and DTOs for size and type

--- libs/foodfolio-infrastructure/src/lib/dao/index.ts
+++ libs/foodfolio-infrastructure/src/lib/dao/index.ts
@@ -1,3 +1,5 @@
 export * from './category.dao';
 export * from './company.dao';
 export * from './location.dao';
+export * from './size.dao';
+export * from './type.dao';
--- libs/foodfolio-infrastructure/src/lib/dto/index.ts
+++ libs/foodfolio-infrastructure/src/lib/dto/index.ts
@@ -1,3 +1,5 @@
 export * from './create-category.dto';
 export * from './create-company.dto';
 export * from './create-location.dto';
+export * from './create-size.dto';
+export * from './create-type.dto';
