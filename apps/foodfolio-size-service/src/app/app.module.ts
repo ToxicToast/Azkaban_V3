e08f24cdeaefe1ecdf3431c31e0c68641f1f80f3
Thomas Kortyka <info@toxictoast.de>
2024-07-01 13:41:38 +0200

    feat: add health checks, metrics and version check

--- apps/foodfolio-size-service/src/app/app.module.ts
+++ apps/foodfolio-size-service/src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { VersionModule } from './version/version.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        HealthModule,
+        MetricsModule,
+        VersionModule,
+    ],
 })
 export class AppModule {}
