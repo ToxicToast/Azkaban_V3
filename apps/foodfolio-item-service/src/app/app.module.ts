c70b008166ebc8d50cccfd4fbf1427e15fd0f953
Thomas Kortyka <info@toxictoast.de>
2024-07-08 11:35:09 +0200

    feat: add Health-, Metrics-, and Version Modules

--- apps/foodfolio-item-service/src/app/app.module.ts
+++ apps/foodfolio-item-service/src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './health/health.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { VersionModule } from './version/version.module';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        HealthModule,
+        MetricsModule,
+        VersionModule,
+    ],
 })
 export class AppModule {}
