5b8c12436a4f8a62703fd825928ff65de11848df
Thomas Kortyka <info@toxictoast.de>
2024-07-16 15:14:03 +0200

    feat: change ui theme and env

--- apps/azkaban-admin-ui/src/features/shared/store/config/config.state.ts
+++ apps/azkaban-admin-ui/src/features/shared/store/config/config.state.ts
@@ -1,6 +1,12 @@
 import { ConfigModel } from './config.model';
 
 export const configState: ConfigModel = {
-    baseUrl: import.meta.env['VITE_AZKABAN_BASE_URL'] ?? '',
-    version: import.meta.env['VITE_AZKABAN_VERSION'] ?? 'v0.0.0',
+    baseUrl:
+        import.meta.env['VITE_AZKABAN_BASE_URL'] ??
+        process.env.VITE_AZKABAN_BASE_URL ??
+        '',
+    version:
+        import.meta.env['VITE_AZKABAN_VERSION'] ??
+        process.env.VITE_AZKABAN_VERSION ??
+        'v0.0.0',
 };
