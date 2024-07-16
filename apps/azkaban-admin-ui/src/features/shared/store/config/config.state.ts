05a2a49aaef50c3e0f049ee2939b3aa1e27937d1
Thomas Kortyka <info@toxictoast.de>
2024-07-16 15:26:59 +0200

    feat: Edit Dockerfile, add Version to Build, Replace baseUrl with hardcoded string

--- apps/azkaban-admin-ui/src/features/shared/store/config/config.state.ts
+++ apps/azkaban-admin-ui/src/features/shared/store/config/config.state.ts
@@ -1,10 +1,7 @@
 import { ConfigModel } from './config.model';
 
 export const configState: ConfigModel = {
-    baseUrl:
-        import.meta.env['VITE_AZKABAN_BASE_URL'] ??
-        process.env.VITE_AZKABAN_BASE_URL ??
-        '',
+    baseUrl: 'https://api-dev.toxictoast.de/api',
     version:
         import.meta.env['VITE_AZKABAN_VERSION'] ??
         process.env.VITE_AZKABAN_VERSION ??
