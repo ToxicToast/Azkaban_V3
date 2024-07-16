07754004e85ad2909e9855f25578c101268dc925
Thomas Kortyka <info@toxictoast.de>
2024-07-16 17:07:38 +0200

    fix: add config module for item details

--- apps/foodfolio-item-detail-service/src/app/app.module.ts
+++ apps/foodfolio-item-detail-service/src/app/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
 import { MetricsModule } from './metrics/metrics.module';
 import { VersionModule } from './version/version.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [HealthModule, MetricsModule, VersionModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        HealthModule,
+        MetricsModule,
+        VersionModule,
+    ],
 })
 export class AppModule {}
