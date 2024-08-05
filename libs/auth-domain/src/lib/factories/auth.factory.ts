85a2354ec13a5b0ab1d5d939be379013980dbde5
Thomas Kortyka <info@toxictoast.de>
2024-08-05 13:44:08 +0200

    feat: update auth infra and domain - add loggedin_at

--- libs/auth-domain/src/lib/factories/auth.factory.ts
+++ libs/auth-domain/src/lib/factories/auth.factory.ts
@@ -5,86 +5,91 @@ import { AuthData } from '../data';
 import { AuthActivationCode, AuthId, AuthPassword } from '../valueObjects';
 
 export class AuthFactory
-    implements Factory<AuthAnemic, AuthAggregate, AuthData>
+	implements Factory<AuthAnemic, AuthAggregate, AuthData>
 {
-    reconstitute(data: AuthAnemic): AuthAggregate {
-        const {
-            id,
-            username,
-            email,
-            password,
-            activation_token,
-            activated_at,
-            banned_at,
-            groups,
-        } = data;
+	reconstitute(data: AuthAnemic): AuthAggregate {
+		const {
+			id,
+			username,
+			email,
+			password,
+			activation_token,
+			activated_at,
+			banned_at,
+			groups,
+			loggedin_at,
+		} = data;
 
-        const userId = new AuthId(id);
-        const userActivationCode = new AuthActivationCode(
-            activation_token,
-            true,
-        );
+		const userId = new AuthId(id);
+		const userActivationCode = new AuthActivationCode(
+			activation_token,
+			true,
+		);
 
-        return new AuthAggregate(
-            userId.value,
-            username,
-            password,
-            email,
-            userActivationCode.value,
-            activated_at,
-            banned_at,
-            groups,
-        );
-    }
+		return new AuthAggregate(
+			userId.value,
+			username,
+			password,
+			email,
+			userActivationCode.value,
+			activated_at,
+			banned_at,
+			loggedin_at,
+			groups,
+		);
+	}
 
-    constitute(data: AuthAggregate): AuthAnemic {
-        const {
-            id,
-            username,
-            password,
-            email,
-            activated_at,
-            activation_token,
-            banned_at,
-            isActive,
-            isBanned,
-            groups,
-        } = data.toAnemic();
+	constitute(data: AuthAggregate): AuthAnemic {
+		const {
+			id,
+			username,
+			password,
+			email,
+			activated_at,
+			activation_token,
+			banned_at,
+			isActive,
+			isBanned,
+			groups,
+			loggedin_at,
+		} = data.toAnemic();
 
-        const userId = new AuthId(id);
-        const userActivationCode = new AuthActivationCode(
-            activation_token,
-            true,
-        );
+		const userId = new AuthId(id);
+		const userActivationCode = new AuthActivationCode(
+			activation_token,
+			true,
+		);
 
-        return {
-            id: userId.value,
-            username,
-            email,
-            password,
-            activation_token: userActivationCode.value,
-            activated_at,
-            banned_at,
-            isActive,
-            isBanned,
-            groups,
-        };
-    }
+		return {
+			id: userId.value,
+			username,
+			email,
+			password,
+			activation_token: userActivationCode.value,
+			activated_at,
+			banned_at,
+			isActive,
+			isBanned,
+			groups,
+			loggedin_at,
+		};
+	}
 
-    createDomain(data: AuthData): AuthAggregate {
-        const { username, password, email } = data;
-        const userId = new AuthId();
-        const activationToken = new AuthActivationCode();
-        const hashedPassword = new AuthPassword(password);
-        return new AuthAggregate(
-            userId.value,
-            username,
-            hashedPassword.value,
-            email,
-            activationToken.value,
-            null,
-            null,
-            [],
-        );
-    }
+	createDomain(data: AuthData): AuthAggregate {
+		const { username, password, email } = data;
+		const userId = new AuthId();
+		const activationToken = new AuthActivationCode();
+		const hashedPassword = new AuthPassword(password);
+		return new AuthAggregate(
+			userId.value,
+			username,
+			hashedPassword.value,
+			email,
+			activationToken.value,
+			null,
+			null,
+			null,
+			[],
+		);
+	}
 }
--- libs/user-infrastructure/src/lib/typeorm/entities/user.entity.ts
+++ libs/user-infrastructure/src/lib/typeorm/entities/user.entity.ts
@@ -1,48 +1,51 @@
 import {
-    Column,
-    CreateDateColumn,
-    DeleteDateColumn,
-    Entity,
-    JoinColumn,
-    OneToMany,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn,
+	Column,
+	CreateDateColumn,
+	DeleteDateColumn,
+	Entity,
+	JoinColumn,
+	OneToMany,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
 } from 'typeorm';
 import { UserGroupEntity } from './user_group.entity';
 
 @Entity({ name: 'azkaban_users' })
 export class UserEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
 
-    @Column({ unique: true, nullable: false, type: 'varchar' })
-    username: string;
+	@Column({ unique: true, nullable: false, type: 'varchar' })
+	username: string;
 
-    @Column({ unique: true, nullable: false, type: 'varchar' })
-    email: string;
+	@Column({ unique: true, nullable: false, type: 'varchar' })
+	email: string;
 
-    @Column({ unique: false, nullable: false, type: 'varchar' })
-    password: string;
+	@Column({ unique: false, nullable: false, type: 'varchar' })
+	password: string;
 
-    @Column({ unique: false, nullable: true, default: null, type: 'varchar' })
-    activation_token: string | null;
+	@Column({ unique: false, nullable: true, default: null, type: 'varchar' })
+	activation_token: string | null;
 
-    @Column({ unique: false, nullable: true, default: null, type: 'timestamp' })
-    activated_at: Date | null;
+	@Column({ unique: false, nullable: true, default: null, type: 'timestamp' })
+	activated_at: Date | null;
 
-    @Column({ unique: false, nullable: true, default: null, type: 'timestamp' })
-    banned_at: Date | null;
+	@Column({ unique: false, nullable: true, default: null, type: 'timestamp' })
+	banned_at: Date | null;
 
-    @CreateDateColumn({ type: 'timestamp' })
-    created_at: Date;
+	@Column({ unique: false, nullable: true, default: null, type: 'timestamp' })
+	loggedin_at: Date | null;
 
-    @UpdateDateColumn({ nullable: true, default: null, type: 'timestamp' })
-    updated_at: Date | null;
+	@CreateDateColumn({ type: 'timestamp' })
+	created_at: Date;
 
-    @DeleteDateColumn({ nullable: true, default: null, type: 'timestamp' })
-    deleted_at: Date | null;
+	@UpdateDateColumn({ nullable: true, default: null, type: 'timestamp' })
+	updated_at: Date | null;
 
-    @OneToMany(() => UserGroupEntity, (group) => group.user)
-    @JoinColumn()
-    groups: Array<UserGroupEntity>;
+	@DeleteDateColumn({ nullable: true, default: null, type: 'timestamp' })
+	deleted_at: Date | null;
+
+	@OneToMany(() => UserGroupEntity, (group) => group.user)
+	@JoinColumn()
+	groups: Array<UserGroupEntity>;
 }
