2eff743904bd725e6fa91b79ed8d8fd99dc7436e
Thomas Kortyka <info@toxictoast.de>
2024-08-08 17:22:19 +0200

    fix: createSize API

--- apps/azkaban-admin-ui/src/features/shared/store/foodfolio/size/size.api.ts
+++ apps/azkaban-admin-ui/src/features/shared/store/foodfolio/size/size.api.ts
@@ -1,7 +1,6 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { dynamicBaseQuery } from '../../dynamicBaseQuery';
 import { FoodFolioLocation } from '@toxictoast/azkaban-sdk';
-import { Nullable } from '@toxictoast/azkaban-base-types';
 import { foodfolioSizeEndpoint } from '../../../../../config/endpoints';
 
 export const sizeApi = createApi({
@@ -17,10 +16,7 @@ export const sizeApi = createApi({
 			providesTags: ['FetchSizes'],
 		}),
 
-		createSize: builder.mutation<
-			void,
-			{ title: string; parent_id: Nullable<string>; freezer: string }
-		>({
+		createSize: builder.mutation<void, { title: string }>({
 			query: (data) => ({
 				url: foodfolioSizeEndpoint,
 				method: 'POST',
