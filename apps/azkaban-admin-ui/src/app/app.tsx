541be0bade6dbb3b92e5ada945771c8086bbbb4c
Thomas Kortyka <info@toxictoast.de>
2024-08-14 11:20:07 +0200

    feat: add refresh token mechanic

--- apps/azkaban-admin-ui/src/app/app.tsx
+++ apps/azkaban-admin-ui/src/app/app.tsx
@@ -3,14 +3,20 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { useAuthState } from '../features/shared/store/auth/auth.hook';
 import { Foodfolio } from './foodfolio';
 import { Azkaban } from './azkaban';
+import { Auth } from './auth';
 
 export function App() {
-	const { isAuthenticated } = useAuthState();
+	const { isAuthenticated, expireTime, refreshToken } = useAuthState();
 
 	return (
 		<ErrorBoundary fallback={<div>Something went wrong</div>}>
 			<Azkaban isAuthenticated={isAuthenticated} />
 			<Foodfolio isAuthenticated={isAuthenticated} />
+			<Auth
+				isAuthenticated={isAuthenticated}
+				expireTime={expireTime ?? 0}
+				refreshToken={() => refreshToken()}
+			/>
 			<Routes isAuthenticated={isAuthenticated} />
 		</ErrorBoundary>
 	);
---
+++ apps/azkaban-admin-ui/src/app/auth.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+interface Props {
+	isAuthenticated: boolean;
+	expireTime: number;
+	refreshToken: () => void;
+}
+
+export function Auth(props: Props) {
+	const { isAuthenticated, expireTime, refreshToken } = props;
+
+	useEffect(() => {
+		if (isAuthenticated) {
+			const interval = setInterval(() => {
+				const dateNow = new Date();
+				const dateExpire = new Date(expireTime * 1000);
+				const diff = dateExpire.getTime() - dateNow.getTime();
+				if (diff <= 10000) {
+					refreshToken();
+				}
+			}, 1000);
+			return () => {
+				clearInterval(interval);
+			};
+		}
+	}, [expireTime, isAuthenticated, refreshToken]);
+
+	return null;
+}
