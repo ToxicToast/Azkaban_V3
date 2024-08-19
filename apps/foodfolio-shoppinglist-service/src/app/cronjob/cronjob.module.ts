49e5777dbe534a128c15899ea83ab81d2bcd356c
Thomas Kortyka <info@toxictoast.de>
2024-08-19 16:37:53 +0200

    feat: add empty products

--- apps/foodfolio-shoppinglist-service/src/app/cronjob/cronjob.module.ts
+++ apps/foodfolio-shoppinglist-service/src/app/cronjob/cronjob.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { CronjobService } from './cronjob.service';
+import {
+	datasourceProvider,
+	itemProvider,
+} from '@azkaban/foodfolio-infrastructure';
 
 @Module({
 	imports: [ScheduleModule.forRoot()],
-	providers: [CronjobService],
+	providers: [...datasourceProvider, ...itemProvider, CronjobService],
 })
 export class CronjobModule {}
--- apps/foodfolio-shoppinglist-service/src/app/cronjob/cronjob.service.ts
+++ apps/foodfolio-shoppinglist-service/src/app/cronjob/cronjob.service.ts
@@ -1,15 +1,41 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { Repository } from 'typeorm';
+import {
+	ItemDAO,
+	ItemEntity,
+	ItemRepository,
+	ItemService as BaseService,
+} from '@azkaban/foodfolio-infrastructure';
 
 @Injectable()
 export class CronjobService {
+	private readonly infrastructureRepository: ItemRepository;
+	private readonly infrastructureService: BaseService;
 	private readonly logger = new Logger(CronjobService.name);
+
+	constructor(
+		@Inject('ITEM_REPOSITORY')
+		private readonly itemRepository: Repository<ItemEntity>,
+	) {
+		this.infrastructureRepository = new ItemRepository(this.itemRepository);
+		this.infrastructureService = new BaseService(
+			this.infrastructureRepository,
+		);
+	}
+
+	private async getProductsWithStockAlert(): Promise<Array<ItemDAO>> {
+		const items = await this.infrastructureService.getItemList();
+		return items.filter((item) => item.isStockAlert);
+	}
 
 	@Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
 		name: 'Empty Products',
 		timeZone: 'Europe/Berlin',
 	})
-	checkForEmptyProducts() {
+	async checkForEmptyProducts(): Promise<void> {
 		this.logger.debug('Checking for empty products');
+		const emptyProducts = await this.getProductsWithStockAlert();
+		this.logger.debug({ ...emptyProducts });
 	}
 }
