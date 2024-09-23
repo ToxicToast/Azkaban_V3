a5bd75cf8ebbfd6ca90cf6002fe59b5efe9509d6
Thomas Kortyka <info@toxictoast.de>
2024-09-23 10:22:54 +0200

    feat: add twitch domain

---
+++ libs/twitch-domain/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+	displayName: 'twitch-domain',
+	preset: '../../jest.preset.js',
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.[tj]s$': [
+			'ts-jest',
+			{ tsconfig: '<rootDir>/tsconfig.spec.json' },
+		],
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: '../../coverage/libs/twitch-domain',
+};
