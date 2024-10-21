148441f9bb1433f6e283b9d35598d2c3da66fbc9
Thomas Kortyka <info@toxictoast.de>
2024-10-21 16:44:32 +0200

    chore: add try catch on cronjobs

--- apps/cronjob-service/src/app/cronjob/user.service.ts
+++ apps/cronjob-service/src/app/cronjob/user.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import {
 	UserTopics,
@@ -14,28 +14,46 @@ export class UsersService {
 	) {}
 
 	private async getAllUsers(): Promise<Array<UserDAO>> {
-		const payload = RmqRecordBuilderHelper({});
-		return await this.userClient.send(UserTopics.LIST, payload).toPromise();
+		try {
+			const payload = RmqRecordBuilderHelper({});
+			return await this.userClient
+				.send(UserTopics.LIST, payload)
+				.toPromise();
+		} catch (e) {
+			Logger.error(e);
+			return [];
+		}
 	}
 
 	private async getUserWithLastLoginLonger2Weeks(): Promise<Array<UserDAO>> {
-		const users = await this.getAllUsers();
-		return users.filter(
-			(user: UserDAO) =>
-				user.isActive &&
-				!user.isBanned &&
-				user.loggedin_at < new Date(Date.now() - 12096e5),
-		);
+		try {
+			const users = await this.getAllUsers();
+			return users.filter(
+				(user: UserDAO) =>
+					user.isActive &&
+					!user.isBanned &&
+					user.loggedin_at < new Date(Date.now() - 12096e5),
+			);
+		} catch (e) {
+			Logger.error(e);
+			return [];
+		}
 	}
 
 	@Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
 		name: 'Inactive Users',
 	})
 	async checkForInactiveUsers(): Promise<void> {
-		const users = await this.getUserWithLastLoginLonger2Weeks();
-		for (const user of users) {
-			const payload = RmqRecordBuilderHelper({ id: user.id });
-			await this.userClient.send(UserTopics.DELETE, payload).toPromise();
+		try {
+			const users = await this.getUserWithLastLoginLonger2Weeks();
+			for (const user of users) {
+				const payload = RmqRecordBuilderHelper({ id: user.id });
+				await this.userClient
+					.send(UserTopics.DELETE, payload)
+					.toPromise();
+			}
+		} catch (e) {
+			Logger.error(e);
 		}
 	}
 }
--- apps/cronjob-service/src/app/cronjob/warcraft.service.ts
+++ apps/cronjob-service/src/app/cronjob/warcraft.service.ts
@@ -6,6 +6,7 @@ import {
 	WarcraftCharacterTopics,
 } from '@toxictoast/azkaban-broker-rabbitmq';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { Nullable } from '@toxictoast/azkaban-base-types';
 
 @Injectable()
 export class WarcraftService {
@@ -18,10 +19,15 @@ export class WarcraftService {
 
 	// TODO: Add Warcraft Character DAO
 	private async getAllCharacters(): Promise<Array<unknown>> {
-		const payload = RmqRecordBuilderHelper({});
-		return await this.characterClient
-			.send(WarcraftCharacterTopics.LIST, payload)
-			.toPromise();
+		try {
+			const payload = RmqRecordBuilderHelper({});
+			return await this.characterClient
+				.send(WarcraftCharacterTopics.LIST, payload)
+				.toPromise();
+		} catch (e) {
+			Logger.error(e);
+			return [];
+		}
 	}
 
 	// TODO: Add Warcraft API DAO
@@ -29,24 +35,33 @@ export class WarcraftService {
 		region: string,
 		realm: string,
 		name: string,
-	): Promise<unknown> {
-		const payload = RmqRecordBuilderHelper({
-			region,
-			realm,
-			name,
-		});
-		return await this.apiClient
-			.send(WarcraftApiTopics.CHARACTER, payload)
-			.toPromise();
+	): Promise<Nullable<unknown>> {
+		try {
+			const payload = RmqRecordBuilderHelper({
+				region,
+				realm,
+				name,
+			});
+			return await this.apiClient
+				.send(WarcraftApiTopics.CHARACTER, payload)
+				.toPromise();
+		} catch (e) {
+			Logger.error(e);
+			return null;
+		}
 	}
 
 	@Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
 		name: 'Update All Warcraft Characters',
 	})
 	async updateCharacters(): Promise<void> {
-		const characters = await this.getAllCharacters();
-		for (const character of characters) {
-			Logger.debug({ character }, 'Updating character');
+		try {
+			const characters = await this.getAllCharacters();
+			for (const character of characters) {
+				Logger.debug({ character }, 'Updating character');
+			}
+		} catch (e) {
+			Logger.error(e);
 		}
 	}
 }
