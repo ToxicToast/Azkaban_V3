1c220ea19a16731b2fe171a861f0a991f97c0dac
Thomas Kortyka <info@toxictoast.de>
2025-01-09 19:51:39 +0100

    chore: use prettier

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -6,12 +6,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
-        HealthModule,
-        MetricsModule,
-        VersionModule,
-        AuthModule,
-    ],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		HealthModule,
+		MetricsModule,
+		VersionModule,
+		AuthModule,
+	],
 })
 export class AppModule {}
